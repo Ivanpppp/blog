3b24c67fc7dbd3bb84535297b1efd7cb6e2c05ff
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2020-11-27 19:37:00 +0800

    1

--- src/main/java/com/ivan/blog/dao/TypeRepository.java
+++ src/main/java/com/ivan/blog/dao/TypeRepository.java
@@ -2,7 +2,9 @@ package com.ivan.blog.dao;
 
 import com.ivan.blog.po.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TypeRepository extends JpaRepository<Type,Long> {
     Type findByName(String name);
 }
--- src/main/java/com/ivan/blog/dao/UserRepository.java
+++ src/main/java/com/ivan/blog/dao/UserRepository.java
@@ -2,7 +2,9 @@ package com.ivan.blog.dao;
 
 import com.ivan.blog.po.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByUsernameAndPassword(String username, String password);
